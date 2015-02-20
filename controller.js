043adb91ed1740883fde3f275d5df23ebcf6870e
Garrett Nordstrom <garrett.a.nordstrom@gmail.com>
2015-02-19 23:20:49 -0700

    second commit with the reddit assignment number two.

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 angular.module('reddit')
 .controller('PostsController', function($scope, firebaseService) {
 	firebaseService.getPosts().then(function(posts) {
+		
 
 	})
 })
--- service.js
+++ service.js
@@ -1,11 +1,10 @@
-var app = angular.madule('reddit');
+var app = angular.module('reddit');
 
 app.service('FirebaseService', function($http, $q){
 	this.getPosts = funciton(){
 		return $http.get('https://devmtn.firebaseio.com/posts.json').then(function(result){
-			return sesult.data;
+			return result.data;
+			
 		})
-
-		
 	}
 })
